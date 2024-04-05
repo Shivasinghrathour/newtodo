3dcac33a4a9f3af0fd447d8176059e9bce3a82cb
officialdigitalbanda <officialdigitalbanda@gmail.com>
2024-04-05 17:45:28 +0530

    color and theme setup

---
+++ lib/config/color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xffABC4E0);
+const secondColor = Color(0xffFF7043);
+
+const textColor = Color(0xffD1D9E6);
+const blueColor = Color(0xff246AFE);
+const lableColor = Color(0xff8C8C8C);
+const whiteColor = Color(0xffFFFFFF);
+const lightGreen = Color(0xffA9FFCA);
+const deepGreen = Color(0xff00B042);
+const deepOrange = Color(0xffFF8A00);
+const lightOrange = Color(0xffFFE091);
+const yellowColor = Color(0xffFECE2F);
---
+++ lib/config/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'color.dart';
+
+var lightTheme = ThemeData(
+  drawerTheme: const DrawerThemeData(backgroundColor: bgColor),
+  scaffoldBackgroundColor: bgColor,
+  useMaterial3: true,
+  appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      backgroundColor: blueColor,
+      iconTheme: IconThemeData(
+        color: textColor,
+      ),
+      titleTextStyle: TextStyle(
+        color: whiteColor,
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      )),
+);
