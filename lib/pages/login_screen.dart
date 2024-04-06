ac1253b14d47abe8457ed8c0b8a90157eaadd236
officialdigitalbanda <officialdigitalbanda@gmail.com>
2024-04-06 21:04:16 +0530

    login and signup page ui is done

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:newtodo/config/color.dart';
@@ -17,159 +19,124 @@ class LoginPage extends StatelessWidget {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Get.back();
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: whiteColor,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Login',
-                    style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 45,
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'Email',
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              EmailField(
-                hint: 'Enter Your Email',
-                icon: Icons.email_outlined,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'Password',
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              PassField(
-                hint: 'Enter Your Password',
-                icon: Icons.lock,
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              PrimaryButton(
-                btnname: 'Login',
-                onPress: () {},
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      color: blueColor,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Text(
-                      "or",
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      color: blueColor,
-                    ),
-                  ),
-                ],
-              ),
-              Spacer(),
-              SecondButton(
-                btnname: 'Login with Google',
-                onPress: () {},
-              ),
-              SecondButton(
-                btnname: 'Login with Apple',
-                onPress: () {},
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: Get.height * .05,
+                ),
+                Row(
                   children: [
-                    Text(
-                      "Don't have an account",
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          fontSize: 16,
-                        ),
+                    IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: secondButtonColor,
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Get.to(SignUpPage());
-                      },
-                      child: Text(
-                        "Register",
-                        style: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              fontSize: 16,
-                              color: blueColor,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    )
                   ],
                 ),
-              )
-            ],
+                SizedBox(height: Get.height * 0.02),
+                Row(
+                  children: [
+                    Text(
+                      'Login',
+                      style: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: Get.height * .03,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Email',
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                EmailField(
+                  hint: 'Enter Your Email',
+                  icon: Icons.email_outlined,
+                ),
+                // EmailField Widget
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Password',
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // PasswordField Widget
+                PassField(
+                  hint: 'Enter Your Password',
+                  icon: Icons.lock,
+                ),
+                SizedBox(height: Get.height * 0.05),
+                // PrimaryButton Widget
+
+                PrimaryButton(
+                  btnname: 'Login',
+                  onPress: () {},
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.to(SignUpPage());
+                        },
+                        child: Text(
+                          "Sign up",
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                                fontSize: 16,
+                                color: blueColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/signup_screen.dart
+++ lib/pages/signup_screen.dart
@@ -1,13 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:newtodo/config/color.dart';
+import 'package:newtodo/config/email_field.dart';
+import 'package:newtodo/config/password_field.dart';
+import 'package:newtodo/config/primary_button.dart';
+import 'package:newtodo/config/second_button.dart';
+import 'package:newtodo/pages/login_screen.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: Get.height * .05,
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: secondButtonColor,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Sign up',
+                      style: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: Get.height * 0.04),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Name',
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                EmailField(
+                    hint: 'Enter Your Name', icon: Icons.account_box_outlined),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Email',
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                EmailField(
+                  hint: 'Enter Your Email',
+                  icon: Icons.email_outlined,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Password',
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                PassField(
+                  hint: 'Enter Your Password',
+                  icon: Icons.lock,
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                PrimaryButton(
+                  btnname: 'Sign up',
+                  onPress: () {},
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Get.to(LoginPage());
+                        },
+                        child: Text(
+                          "Login",
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                                fontSize: 16,
+                                color: blueColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
